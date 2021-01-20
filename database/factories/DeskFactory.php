2016f830cc49ea17ea610ffcbced5a506bd6c649
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2021-01-20 20:57:21 +0600

    adaptive pages

--- database/factories/DeskFactory.php
+++ database/factories/DeskFactory.php
@@ -13,6 +13,7 @@ $factory->define(Desk::class, function (Faker $faker) {
         'user_id' => User::all()->random(),
         'program_id' => Program::all()->random(),
         'balance' => rand('10', '50000'),
+        'code' => $faker->unique()->randomNumber(6),
         'is_closed' => rand(0, 1),
         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
